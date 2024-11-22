f96b88be24bb30421bf93e3124f6768890c46eb9
Rochelle Charles <roccharl@amazon.co.uk>
2024-11-22 15:48:34 +0000

    MinHeap

--- src/main/java/org/example/LinearDataStructures/NonLinearDataStructures/Trees/Heaps/MinHeap.java
+++ src/main/java/org/example/LinearDataStructures/NonLinearDataStructures/Trees/Heaps/MinHeap.java
@@ -32,11 +32,34 @@ public class MinHeap {
         this.bubbleUp();
     }
 
+    //popMin() to remove the minimum value
+    //can only remove an element with no children
+    //so min needs to be swapped to the last element
+    public int popMin() {
+        //if statement checks if heap is empty and throws error if is
+        if (this.size == 0) {
+            throw new Error("Heap is empty");
+        }
+        System.out.println("Swap min element " + this.heap.get(1) + " and last element " + this.heap.get(this.size));
+        //swaps min element with last element ready for removing
+        this.swap(1, this.size);
+
+        //removes element using ArrayList .remove()
+        int min = this.heap.remove(this.size);
+        System.out.println("Removed from the heap: " + min);
+
+        System.out.println(this.heap);
+        //decreases heap size to reflect the min element removal
+        this.size--;
+        return min;
+
+    }
+
     //bubbleUp() ensures the heap always maintains smallest element at the root
     private void bubbleUp() {
         // Instantiate current storing last inndex of this.heap (always the locaiton of the added element)
         int current = this.size;
-
+        int swapCount = 0;
         // 2 conditions 1. any index greater than 1, 2. value at current is less than its parent value
         while (current > 1 && this.heap.get(current) < this.heap.get(this.getParent(current))) {
             //swap elements to restore the heap
@@ -45,6 +68,40 @@ public class MinHeap {
             //call to swap method
             this.swap(current, this.getParent(current));
             current = this.getParent(current);
+            if (this.size == 10000) {
+                System.out.println("A heap of " + this.size + " elements was restored with only " + swapCount + " swaps!");
+            }
+        }
+    }
+
+    //heapify method to shift elements down the tree after a swap and removal
+    private void heapify() {
+        int current = 1;
+        int leftChild = this.getLeft(current);
+        int rightChild = this.getRight(current);
+        int swapCount = 0;
+        while (this.canSwap(current, leftChild, rightChild)) {
+            if (this.exists(leftChild) && this.exists(rightChild)) {
+                if (this.heap.get(leftChild) < this.heap.get(rightChild)) {
+                    this.swap(current, leftChild);
+                    current = leftChild;
+                    swapCount++;
+                } else {
+                    this.swap(current, rightChild);
+                    current = rightChild;
+                    swapCount++;
+                }
+
+            } else {
+                this.swap(current, leftChild);
+                current = leftChild;
+                swapCount++;
+            }
+            leftChild = this.getLeft(current);
+            rightChild = this.getRight(current);
+        }
+        if (this.size == 9999) {
+            System.out.println("A heap of " + this.size + " elements was restored with only " + swapCount + " swaps!");
         }
     }
 
@@ -52,6 +109,15 @@ public class MinHeap {
         int temp = this.heap.get(b);
         this.heap.set(b, this.heap.get(a));
         this.heap.set(a, temp);
+    }
+
+    private boolean exists(int index) {
+        return index <= this.size;
+    }
+
+    private boolean canSwap(int current, int leftChild, int rightChild) {
+        return (this.exists(leftChild) && (this.heap.get(current) > this.heap.get(leftChild)))
+                || (this.exists(rightChild) && (this.heap.get(current) > this.heap.get(rightChild)));
     }
 
     //child and parent elements are determined by their relative indices within heap.
@@ -112,6 +178,23 @@ public class MinHeap {
         // Display the heap after bubbling up
         System.out.println("-------------");
         System.out.println("BUBBLED UP: " + minHeap4.heap);
+
+        //remove the minimum value
+        minHeap.popMin();
+
+        System.out.println("\n");
+
+        MinHeap minHeap7 = new MinHeap();
+
+        // Populate minHeap with descending #s from 10001 to 1
+        System.out.println("Adding 10000 individual elements...");
+        for (int i = 10000; i >= 1; i--) {
+            minHeap7.add(i);
+        }
+        // Remove minimum value from minHeap
+        System.out.println("Removing the minimum value...");
+        System.out.println("Minimum value: " + minHeap7.popMin());
+    }
 
     }
 
